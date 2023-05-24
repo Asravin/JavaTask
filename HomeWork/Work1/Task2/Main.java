cac10301c142938a477a6974991b824bfc404c74
Asravin <maisweedfell@.com>
2023-05-24 16:39:38 +0400

    Простые числа

---
+++ HomeWork/Work1/Task2/Main.java
@@ -0,0 +1,21 @@
+package HomeWork.Work1.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 1000;
+        for(int i = 2; i < number; i++) {
+            boolean primal = true;
+            int num = (int) Math.sqrt(i);
+            for(int j = 2; j <= num; j++) {
+                if((i % j) == 0) {
+                    primal = false;
+                    break;
+                }
+            }
+
+            if(primal) {
+                System.out.print(i + " ");
+            }
+        }
+   }
+}
